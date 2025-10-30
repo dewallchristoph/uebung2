b97ad9ca4655e4bf2e50ee7328a0856038e13a62
Christoph  de Wall <christophdewall@Mac.fritz.box>
2025-10-30 16:43:24 +0100

    h1: bedingte subtraktion

---
+++ src/h1/H1_main.java
@@ -0,0 +1,17 @@
+package h1;
+
+public class H1_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int z = 5; // beliebiger Startwert
+
+		if (z > 10) {
+		    z = z - 10;
+		}
+
+		System.out.println("z = " + z);
+		}
+
+}
